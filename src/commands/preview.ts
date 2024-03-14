5e300074595945a9235a138bc2b391b987a61856
Alex <naplle58@gmail.com>
2024-03-14 22:36:55 +0300

    markdown render preview

--- src/commands/preview.ts
+++ src/commands/preview.ts
@@ -1,15 +1,32 @@
+import MarkdownIt from "markdown-it"
+import { NodeHtmlMarkdown } from "node-html-markdown"
 import * as vscode from "vscode"
 import { getQuestionContent } from "../api/question_content"
 import { Question } from "../models"
 
+const htmlToMD = new NodeHtmlMarkdown({ maxConsecutiveNewlines: 2 })
+const md = MarkdownIt()
+
 export const preview = async (q: Question) => {
-    const webview = vscode.window.createWebviewPanel("preview", `Preview: ${q.title}`, vscode.ViewColumn.Beside, {})
+    const webview = vscode.window.createWebviewPanel(
+        "preview",
+        `Preview: ${q.title}`,
+        vscode.ViewColumn.Beside,
+        {},
+    )
 
     const questionContent = await getQuestionContent(q.titleSlug)
-
     if (questionContent?.content) {
-        webview.webview.html = questionContent.content
+        let previewContent = htmlToMD.translate(questionContent.content)
+        previewContent = updateText(q,previewContent)
+        webview.webview.html = md.render(previewContent)
     } else {
-        vscode.window.showErrorMessage("Can't preview ")
+        vscode.window.showErrorMessage(`Can't preview ${q.title}`)
     }
+}
+
+const updateText=(q:Question, md:string):string => {
+    return `
+# ${q.title}
+` + md
 }
